3357e8d25cff0ac7748f0c89ea5fa5ff893ff3bc
Ezra Gubbay <ezragubbay99@gmail.com>
2026-01-03 00:21:33 +0200

    Added user ID validation and updated server code to ASGI architecture

--- backend/server.py
+++ backend/server.py
@@ -1,198 +1,354 @@
-from flask import Flask, jsonify, request
-from flask_socketio import SocketIO, emit, disconnect
+import os, shutil
 import socketio
+from typing import List, Union, Dict
+from fastapi import FastAPI, Request, BackgroundTasks, HTTPException, UploadFile, File, Form
+from fastapi.concurrency import run_in_threadpool
+from pydantic import BaseModel, ValidationError
+from dotenv import load_dotenv
+from redis import asyncio as redis
+
 from llm_service import LLMClient
 from db import DBManager
 from firebase import generate_notification
-import threading
 from data_types import MessageType
-from dotenv import load_dotenv
-import os
 
+# Load environment variables
 load_dotenv()
 LLM_MODEL = os.getenv("LLM_MODEL")
-CHAT_SIZE_LIMIT = 300
-TEST_MOBILE_FCM_TOKEN = os.getenv("IPHONE_FCM_TOKEN")
+CHAT_SIZE_LIMIT = os.getenv("CHAT_SIZE_LIMIT")
 APP_IDENTIFIER = os.getenv("APP_IDENTIFIER")
-app = Flask(__name__)
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+UPLOAD_DIR = os.getenv("UPLOAD_DIR", "/app/temp_storage")
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    message_queue='redis://127.0.0.1:6379',
-    async_mode='eventlet',
-    max_http_buffer_size=10 * 1024 * 1024
+# ----- SETUP & INFRASTRUCTURE -----
+
+# Async Redis Manager - multiple worker communicating to connected users via Redis
+mgr = socketio.AsyncRedisManager(REDIS_URL)
+
+# ASGI Socket.IO Server
+sio = socketio.AsyncServer(
+    async_mode="asgi",
+    cors_allowed_origins='*',
+    client_manager=mgr,
+    ping_timeout=60,
+    ping_interval=25,
 )
 
+# FastAPI App
+app = FastAPI()
+
+# Combine ASGI app and FastAPI app for Gunicorn service
+combined = socketio.ASGIApp(sio, app)
+
+# Initialize DB
 dbmanager = DBManager()
-client = LLMClient(model=LLM_MODEL, chat_size_limit=CHAT_SIZE_LIMIT)
 
-connected_users = {}
+# Global LLM Client
+llm_client = LLMClient(model=LLM_MODEL, chat_size_limit=CHAT_SIZE_LIMIT)
 
-"""
-API:
-1. Connect
-2. Identify
-3. Disconnect
-4. Send prompt to LLM
-"""
-@socketio.on('connect')
-def connect(auth):
-    if not auth:
-        auth = request.args
-        
-    uid = auth.get("uid")
-    print(f'Received Auth Info: {auth}')
+# ----- DATA MODELS -----
 
-    if uid:
-        message_queue = dbmanager.dequeueMessageQueue(uid)
-        print(message_queue)
-        if message_queue:
-            print(f"[DEBUG-CONNECT] Messages found in user {uid} message queue. Unloading to client...")
-            for payload in message_queue:
-                print(f"[DEBUG-CONNECT] Unloading message: {payload}")
-                socketio.emit('llm_response', {'status': 'success', 'response': payload}, to=request.sid)
-    else:
-        print(f"[SOCKET-CONNECT] New Anonymous connection @ {request.sid} (Waiting for Login)")
-        return True
+class AuthPayload(BaseModel):
+    username: str
+    passwordHash: str
 
-    connected_users[uid] = request.sid
-    print(f"[SOCKET-CONNECT] New connection from User: {uid} @ {request.sid}")
+class SendMessageRequest(BaseModel):
+    uid: str
+    prompt: str
+    notificationToken: Union[str, None] = None
+    
+class RegisterNotificationTokenRequest(BaseModel):
+    uid: str
+    notificationToken: str
 
-@socketio.on('disconnect')
-def disconnect():
-    for uid, sid, in list(connected_users.items()):
-        if sid == request.sid:
-            del connected_users[uid]
-            print(f"[SOCKET-DISCONNECT] Application backgrounded or closed by User: {uid} @ {sid}")
+class ResetChatRequest(BaseModel):
+    uid: str
 
-@socketio.on('registerUser')
-def registerUser(data):
-    print("Received user registration request")
+# ----- HELPER FUNCTIONS -----
 
-    username = data.get('username')
-    passhash = data.get("passhash")
-    status, response = dbmanager.create_user(username=username, passhash=passhash)
+async def is_user_connected(uid: str):
+    """
+    Checks Redis to see if user is connected to any worker.
+    """
 
-    if status:
-        socketio.emit('auth_response', {'status': 'success', 'response': response}, to=request.sid)
-        print("Registration successful. DB Response: ", response)
-    else:
-        socketio.emit('auth_response', {'status': 'error', 'response': response}, to=request.sid)
-        print('Failed to register user. DB Response: ', response)
-
-@socketio.on('login')
-def login(auth):
-
-    username = auth.get('username')
-    passhash = auth.get("passhash")
-    status, response = dbmanager.login(username=username, passhash=passhash)
-    if status:
-        socketio.emit('auth_response', {'status': 'success', 'uid': response}, to=request.sid)
-    else:
-        socketio.emit('auth_response', {'status': 'error', 'uid': response}, to=request.sid)
-
-@app.route('/sendMessage', methods=['POST'])
-def send_message():
-    data = request.get_json()
-    print("Received send message request from user")
-        
-    uid = data.get("uid")
-    event = 'llm_response'
-    notificationToken = data.get("notificationToken")
-
-    if not uid:
-        print(f"[CLIENT-ERR] No user ID provided in call to sendMessage")
+    # Check if room user:{uid} has any participants
+    try:
+        participants = await sio.manager.get_participants(namespace='/', room=uid)
+        return True if participants else False
+    except Exception as e:
+        print(f"[WARN] Failed to check user connection status: {e}")
         return False
 
-    print("Starting background task to process LLM response")
-    llm_result = client.sendMessage(prompt=data.get("prompt"), attachments=data.get('attachments'))
-    payload = {
-        'message': llm_result,
+def validate_socket_model(model_class):
+    def decorator(func):
+        async def wrapper(sid, data):
+            try:
+                obj = model_class(**data)
+            except ValidationError as e:
+                print("[ERROR] Invalid Data:")
+                print(f"Data Received: {data}")
+                print(f"Validation Error: {e}")
+                response = {
+                    'message': 'Validation Error 422: Invalid Request Data',
+                    'type': MessageType.System
+                }
+
+                # Let client know of error. Emit to sid 
+                await sio.emit('error', {'response': response}, room=sid)
+                return
+            return await func(sid, obj)
+        return wrapper
+    return decorator
+
+def notify(uid: str, response: str):
+    """
+    Sends a notification to the user.
+    """
+
+    # Get notification token from database
+    notificationToken = await run_in_threadpool(dbmanager.getNotificationToken, uid)
+
+    # Construct notification payload
+    notification = {
+        'title': "PAgent",
+        'body': response.get("message"),
+        'data': {
+            'app': APP_IDENTIFIER,
+            'type': response.get('type')
+        }
+    }
+
+    # Send notification to Firebase
+    await run_in_threadpool(generate_notification, notificationToken, notification)
+
+# ----- SOCKET.IO EVENTS -----
+
+@sio.event
+async def connect(sid, environ, auth):
+    """
+    Handles new connections to the server.
+    """
+
+    uid = auth.get("uid") if auth else None
+    if uid:
+        await sio.enter_room(sid, uid)
+        print(f"[CONNECT] SID: {sid} | UID: {uid}")
+
+        message_queue = await run_in_threadpool(dbmanager.dequeueMessageQueue, uid)
+        if message_queue:
+            print(f"[MESSAGE-QUEUE] Flushing {len(message_queue)} messages to user: {uid}")
+            for payload in message_queue:
+                await sio.emit('llm_response', {'status': 'success', 'response': payload}, room=uid)
+    else:
+        print(f"[CONNECT-ANON] Anonymous connection @ {sid} (Waiting for Login)")
+    
+@sio.event
+async def disconnect(sid):
+    """
+    Notifies the server of a client disconnecting.
+    Removal of user from Redis and database is handled automatically by Socket.IO.
+    """
+
+    print(f"[DISCONNECT] SID: {sid}")
+
+@app.post('/registerUser')
+async def registerUser(data: AuthPayload):
+    """
+    Handles new user registration.
+    """
+
+    username = data.get("username")
+    passwordHash = data.get("passwordHash")
+    
+    status, response = await run_in_threadpool(dbmanager.create_user, username=username, password=passwordHash)
+
+    return {'status': 'success' if status else 'error', 'response': response}
+
+@app.post('/login')
+async def login(data: AuthPayload):
+    """
+    Handles user logins.
+    """
+
+    username = data.username
+    passwordHash = data.passwordHash
+
+    status, response = await run_in_threadpool(dbmanager.login, username=username, passwordHash=passwordHash)
+
+    return {'status': 'success' if status else 'error', 'response': response}
+
+# ----- API ROUTES -----
+
+@sio.on('sendMessage')
+@validate_socket_model(SendMessageRequest)
+async def sendMessage(sid, req):
+    """
+    Handles asynchronous processing of user prompts to the llm.
+    data argument is expected to carry user ID (uid), prompt, and optionally, attachments.
+
+    Standard API method for in-app communication with user.
+    """
+    sio.emit('llm_processing', {'status': 'success', 'response': 'Thinking...'}, room=req.uid)
+    await process_llm_request(req)
+
+@app.post('/sendMessage')
+async def sendMessage(req: SendMessageRequest, background_tasks: BackgroundTasks):
+    """
+    Handles asynchronous processing of user prompts to the llm.
+    data argument is expected to carry user ID (uid), prompt, and optionally, attachments.
+
+    Used for when user replies to LLM from outside the app, e.g. through push notification.
+    """
+    background_tasks.add_task(process_llm_request, req)
+    return {'status': 'success'}
+
+async def process_llm_request(req: SendMessageRequest):
+    """
+    Prepares user request to the LLM and calls the LLM before responding to user.
+
+    1. Decomposes user request into user ID, prompt, and attachments.
+    2. Gathers all attachments uploaded by the user (if any).
+    3. Makes asynchronous call to LLMClient.
+    4. Emits response to user.
+    """
+
+    uid, prompt = req.uid, req.prompt
+
+    # Collect attachments if user uploaded any.
+    attachments = []
+    user_dir = os.path.join(UPLOAD_DIR, uid)
+    if os.path.exists(user_dir):
+        for filename in os.listdir(user_dir):
+            file_path = os.path.join(user_dir, filename)
+            attachments.append({
+                "path": file_path,
+                "name": filename
+            })
+
+    # Use asynchronous LLMClient
+    print(f"[LLM-START] Processing LLM request for user {uid}")
+    message = await run_in_threadpool(
+        llm_client.sendMessage,
+        uid,
+        prompt,
+        attachments
+    )
+
+    response = {
+        'message': message,
         'type': MessageType.Assistant
     }
-    
-    if uid in connected_users:
-        print(f"[MSG-DIRECT] User {uid} is foregrounded - sending via WebSocket. ")
-        print(f"Emitting to user: {uid} the following response from LLM: {llm_result}")
-        socketio.emit(event, {'response': payload}, to=connected_users[uid])
+
+    # --- CLEANUP USER TEMP UPLOAD FILES ---
+    if os.path.exists(user_dir):
+        try:
+            shutil.rmtree(user_dir)
+            print(f"[USERDIR-CLEANUP] Cleaned temporary user upload files for {uid}")
+        except Exception as e:
+            print(f"[USERDIR-ERR] Error cleaning temporary user upload files for {uid}:\n{e}")
+
+    # --- DELIVERY ---
+
+    if await is_user_connected(uid):
+        # If user is connected, emit response directly to user.
+        await sio.emit('llm_response', {"response": response}, room=uid)
     else:
-        print(f"[MSG-NOTIFY] User {uid} is backgrounded - sending via Web Push. ")
-        dbmanager.insertMessageQueue(uid=uid, payload=payload)
-
-        print(data.get('notify'))
-        if data.get('notify'):
-            notification = {
-                'title': "PAgent",
-                'body': llm_result if len(llm_result) < 50 else llm_result[:50],
-                'data': {
-                    'app': APP_IDENTIFIER,
-                    'type': payload.get('type'),
-                }
-            }
-            generate_notification(notificationToken, notification)
-    return {'status': 'success', 'response': payload}
-
-@app.route('/loadNewChat', methods=['POST'])
-def loadNewChat():
-    global client
-    data = request.get_json()
-    uid = data.get('uid')
-    print(f'[SRV-DBG] Received remoteChatErase request from user {uid}')
-    if not uid:
-        print(f"[CLIENT-ERR] No user ID provided in call to loadNewChat")
-        return {'status': 'error', 'response': 'No valid UID provided'}
-    
-    elif dbmanager.isValidUserID(uid):
-        client = LLMClient(model=LLM_MODEL, chat_size_limit=CHAT_SIZE_LIMIT)
-        return {'status': 'success', 'response': 'Successfully loaded new chat'}
-    else:
-        print(f"[CLIENT-ERR] Invalid user ID provided in call to loadNewChat")
-        return {'status': 'error', 'response': 'No valid UID provided'}
-
-@socketio.on('registerNotificationToken')
-def registerNotificationToken(data):
-    if not data:
-        data = request.args
+        # If user is not connected, insert response into message queue and send notification.
+        status, response = await run_in_threadpool(dbmanager.insertMessageQueue, uid=uid, message=response)
+        if not status:
+            raise HTTPException(status_code=500, detail="Failed to insert message queue")
         
-    uid = data.get("uid")
+        # Send notification to user.
+        await notify(uid, payload=response)
 
-    if not uid:
-        print(f"[CLIENT-ERR] No user ID provided in call to registerNotificationToken")
-        return False
-    token = data.get('notificationToken')
-    dbresult = dbmanager.addNotificationToken(uid, token)
+@app.post('/uploadFileObject')
+async def upload_file_object(
+    uid: str = Form(...),           # User ID (expected as form field)
+    file: UploadFile = File(...),   # File (expected as file stream)
+):
+    """
+    Handles file uploads for the user's prompt to the llm.
 
-    message_type = 'notification token registration'
-    if dbresult:
-        respond(
-            message_type=message_type,
-            uid=uid,
-            sid=request.sid,
-            payload={'message': "Successfully registered for PAgent notifications!", 'type': MessageType.System}
-        )
+    These files will be processed and sent to the llm as part of the user's next prompt.
+    Multipart Upload to Shared Storage (streamed and uses shared K8s volume)
+    """
+
+    # ----- SECURITY CHECKS ----- 
+    
+    # Ensure user exists and UID is
+    if not await run_in_threadpool(dbmanager.isValidUserID, uid):
+        raise HTTPException(status_code=400, detail="Invalid user ID")
+
+    # Ensure safe filename to prevent directory traversal attacks
+    safe_filename = os.path.basename(file.filename)
+    user_dir = os.path.join(UPLOAD_DIR, uid)
+
+    # Create user directory if it doesn't exist
+    os.makedirs(user_dir, exist_ok=True)
+
+    file_path = os.path.join(user_dir, safe_filename)
+    
+    # Stream file to disk
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {'status': 'success', 'filename': safe_filename}
+
+@app.post('/deleteFileObject')
+async def delete_file_object(
+    uid: str = Form(...),           # User ID (expected as form field)
+    filename: str = Form(...),      # File name (expected as form field)
+):
+    """
+    Handles file deletion for the user's prompt to the llm.
+    """
+
+    # ----- SECURITY CHECKS ----- 
+    
+    # Ensure user exists and UID is
+    if not await run_in_threadpool(dbmanager.isValidUserID, uid):
+        raise HTTPException(status_code=401, detail="Authentication Error: Invalid user ID")
+
+    # Ensure safe filename to prevent directory traversal attacks
+    safe_filename = os.path.basename(filename)
+    user_dir = os.path.join(UPLOAD_DIR, uid)
+
+    file_path = os.path.join(user_dir, safe_filename)
+
+    # Check if file exists
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return {'status': 'success', 'filename': safe_filename}
     else:
-        socketio.emit(message_type, {'response': "Error registering user for notifications"}, to=request.sid)
+        raise HTTPException(status_code=404, detail="File not found")
 
+@app.post('/loadNewChat')
+async def load_new_chat(req: ResetChatRequest):
+    """
+    Deletes old user chat history and creates a new one.
+    """
 
-def respond(message_type, uid, sid, payload):
+    uid = req.uid
+    if not await run_in_threadpool(dbmanager.isValidUserID, uid):
+        raise HTTPException(status_code=401, detail="Authentication Error: Invalid user ID")
 
-    message_type = 'notification token registration'
-    if sid in connected_users:
-        print(f"[MSG-DIRECT] User {uid} is foregrounded - sending via WebSocket. ")
-        socketio.emit(message_type, {'response': payload}, to=connected_users[uid])
-    else:
-        print(f"[MSG-NOTIFY] User {uid} is backgrounded - sending via Web Push. ")
+    await run_in_threadpool(llm_client.clear_history, req.uid)
+    return {'status': 'success'}
 
-        notification = {
-            'title': "PAgent",
-            'body': payload.get('message'),
-            'data': {
-                'app': APP_IDENTIFIER,
-                'type': payload.get('type'),
-            }
-        }
-        generate_notification(notificationToken, notification)
-
-# if __name__ == '__main__':
-    # socketio.run(app, host='0.0.0.0', port=8000)
+@sio.on('registerNotificationToken')
+@validate_socket_model(RegisterNotificationTokenRequest)
+async def register_notification_token(sid, req: RegisterNotificationTokenRequest):
+    """
+    Registers a user's notification token.
+    """
+    response = await run_in_threadpool(dbmanager.addNotificationToken, req.uid, req.token)
+    await sio.emit(
+        'notificationTokenRegistrationResponse', 
+        {
+            'status': 'success' if response else 'error',
+            'message': f'Successfully registered notification token {req.token}'
+                if response else f'Error registering notification token for user {req.uid}'
+        },
+        room=req.uid
+    )
+                
