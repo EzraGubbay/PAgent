e19eeb0e99f0796a514b781ba5d0ee6e97af48ab
Ezra Gubbay <ezragubbay99@gmail.com>
2026-01-29 01:41:41 +0200

    Set up unit tests - sending messages to backend, Auth Screen functionality; unified project gitignore files

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   preset: "jest-expo",
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|msw|@msw|until-async)"
   ],
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  testEnvironmentOptions: {
+    customExportConditions: [''],
   }
 };
